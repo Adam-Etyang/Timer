bd8e81d8c01d9eb3d40252cb03e9a6a8c85522de
Adam-Etyang <adametyang69@icloud.com>
2026-01-07 19:52:12 +0300

    Broken CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import './App.css'
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [minuites, setMinuites] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  return (
+    <div className='container'>
+      {/*setting time page*/}
+      <div className='time-container'>
+        <div className='hour-container'>
+          <span className='hr-tag'><p>hr</p></span>
+          <span className='seconds'><p>{seconds}</p></span>
+        </div>
+        <div className='minuite-container'>
+          <span className='time'><p>{minuites}</p></span>
+        </div>
+        <div className='seconds-container'>
+          <span><p className='time'>{hours}</p></span>
+        </div>
+      </div>
+      <div className='buttons-container'>
+        <div className='cancel-button'></div>
+        <div className='start-button'></div>
+      </div>
+
+    </div>
+  );
+
+
 }
 
-export default App
